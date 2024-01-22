bb93a34e01b697848b7fc5428ce9b0195a0c5dc8
ndaDayo <hayato711+nda@gmail.com>
2024-01-22 22:25:53 +0900

    add: run fn

---
+++ Chapter3/catr/tests/cli.rs
@@ -0,0 +1,48 @@
+use assert_cmd::Command;
+use predicates::prelude::predicate;
+use rand::{distributions::Alphanumeric, Rng};
+use std::error::Error;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn Error>>;
+
+const PRG: &str = "catr";
+const EMPTY: &str = "tests/inputs/empty.txt";
+const FOX: &str = "tests/inputs/fox.txt";
+const SPIDERS: &str = "tests/inputs/spiders.txt";
+const BUSTLE: &str = "tests/inputs/the-bustle.txt";
+
+#[test]
+fn skips_bad_file() -> TestResult {
+    let bad = gen_bad_file();
+    let expected = format!("{}: .*[(]os error 2[)]", bad);
+    Command::cargo_bin(PRG)?
+        .arg(&bad)
+        .assert()
+        .success()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}
+
+fn gen_bad_file() -> String {
+    loop {
+        let filename: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+        if fs::metadata(&filename).is_err() {
+            return filename;
+        }
+    }
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin(PRG)?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
