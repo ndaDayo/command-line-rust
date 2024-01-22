29662ac7a4dd2fa9eafc622a92f6082014a4b27e
ndaDayo <hayato711+nda@gmail.com>
2024-01-22 15:22:50 +0900

    mod: use TestResult

--- Chapter2/echor/tests/cli.rs
+++ Chapter2/echor/tests/cli.rs
@@ -3,12 +3,14 @@ use std::fs;
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 #[test]
-fn dies_no_args() {
+fn dies_no_args() -> TestResult {
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
@@ -18,9 +20,10 @@ fn runs() {
 }
 
 #[test]
-fn hello1() {
+fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
     let expected = fs::read_to_string(outfile).unwrap();
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
