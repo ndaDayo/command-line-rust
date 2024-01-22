dc9e54f88933f252c337982c35ea4c474d5edcc7
ndaDayo <hayato711+nda@gmail.com>
2024-01-22 15:32:28 +0900

    add: args tests

--- Chapter2/echor/tests/cli.rs
+++ Chapter2/echor/tests/cli.rs
@@ -6,7 +6,7 @@ use predicates::prelude::*;
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 #[test]
 fn dies_no_args() -> TestResult {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
@@ -22,8 +22,19 @@ fn runs() {
 #[test]
 fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let expected = fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello2.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(vec!["Hello", "there"])
+        .assert()
+        .success()
+        .stdout(expected);
     Ok(())
 }
