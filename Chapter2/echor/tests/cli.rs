bc0b9fabce8c68e916731ef6975103a628c8d812
ndaDayo <hayato711+nda@gmail.com>
2024-01-22 15:37:37 +0900

    mod: run helper fn

--- Chapter2/echor/tests/cli.rs
+++ Chapter2/echor/tests/cli.rs
@@ -13,19 +13,19 @@ fn dies_no_args() -> TestResult {
     Ok(())
 }
 
-#[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
 }
 
 #[test]
 fn hello1() -> TestResult {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello there"], "tests/expected/hello1.txt")
 }
 
 #[test]
