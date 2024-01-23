02a9965e38ffec0e90deebdf87f2714512cc9f69
ndaDayo <hayato711+nda@gmail.com>
2024-01-23 12:21:09 +0900

    add: parse_positive_int

--- Chapter4/headr/src/lib.rs
+++ Chapter4/headr/src/lib.rs
@@ -23,3 +23,27 @@ pub fn get_args() -> MyResult<Config> {
         bytes,
     })
 }
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    unimplemented!();
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
